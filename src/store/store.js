1bf9a3982ccc513c52c8eb147e218b798fbcb5c2
suhuashan <2891013025@qq.com>
2019-11-26 23:39:34 +0800

    [feature] 加入vuex的strict属性

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,11 @@ import Vuex from 'vuex'
 import moduleA from './modules/module_a'
 import moduleB from './modules/module_b'
 
+let isDev = process.env.NODE_ENV === 'development'
+
 export default () => {
   return new Vuex.Store({
+    strict: isDev,
     modules: {
       moduleA,
       moduleB
